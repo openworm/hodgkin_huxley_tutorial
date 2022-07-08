2b399e648a488adf29dde6cda358fec73071b12e
Rahul <rahulsonkar91@gmail.com>
2022-07-08 04:04:07 +0000

    suggested changes

--- notebook/ui_widget.py
+++ notebook/ui_widget.py
@@ -16,26 +16,33 @@ def resetTodefault(_):
     slider_amplitude.value   = 50
     slider_width.value       = 0
     slider_translation.value = 50
-    cb.value                 = False
-
-#function to define injector current parameters and plot
-def injectorCurrent(amplidute=10, t_width=100, t_translation=100):
-    t    = np.arange(0, 450, 5)  #range for plotting only not used in simulation
-    for i in t:
-        I_inj = amplidute*(t>t_translation) - amplidute*(t>t_translation+t_width)
-    plt.figure()
-    plt.plot(t, I_inj)
-    plt.xlabel('t (ms)', fontsize = 15)
-    plt.ylabel('$I_{inj}$ ($\\mu{A}/cm^2$)')
-    plt.show()
-    #display(plt.show(),wid_plotArea)
     
-header_capacitance = ipywidgets.HTML(value=f"<b><font color='blue'>Membrane Capacitance, uF/cm^2</b>")
-header_conductance = ipywidgets.HTML(value=f"<b><font color='blue'>Maximum Conductances, mS/cm^2</b>")
-header_potential   = ipywidgets.HTML(value=f"<b><font color='blue'>Nernst Reverasal Potentials, mV</b>")
-header_simTime     = ipywidgets.HTML(value=f"<b><font color='blue'>Simulation Time, ms</b>")
+#function to change slider handle colour when move from default
+def highlight_slider():
+    inputList    = [slider_capacitance, slider_cond_Na, slider_cond_K, slider_cond_L, slider_pot_Na, slider_pot_K, slider_pot_L]
+    inputDefault = [1.0, 120, 36, 0.3, 50, -77, -54.387]
+    for l, d in zip(inputList,inputDefault):
+        if l.value == d:
+            l.style.handle_color = 'white'
+        else:
+            l.style.handle_color = 'orange'
+    if slider_width.value == 0.0:
+        slider_amplitude.style.handle_color = 'white'
+        slider_width.style.handle_color = 'white'
+        slider_translation.style.handle_color = 'white'
+    else:
+        slider_amplitude.style.handle_color = 'orange'
+        slider_width.style.handle_color = 'orange'
+        slider_translation.style.handle_color = 'orange'
+            
+header_capacitance = ipywidgets.HTMLMath(value=r"<b><font color='blue'>Membrane Capacitance, \(\mu{F}/cm^2\)</b>")
+header_conductance = ipywidgets.HTMLMath(value=r"<b><font color='blue'>Maximum Conductances, \(mS/cm^2\)</b>")
+header_potential   = ipywidgets.HTMLMath(value=r"<b><font color='blue'>Nernst Reverasal Potentials, \(mV\)</b>")
+header_simTime     = ipywidgets.HTMLMath(value=r"<b><font color='blue'>Simulation Time, \(ms\)</b>")
+header_injCurrent  = ipywidgets.HTMLMath(value=r"<b><font color='blue'>Injection Current, \(\mu{A}/cm^2\)</b>")
+injCurrent_note    = ipywidgets.HTML(value=f"<i>*For injection current width = 0, the model uses default two pulse signal from tutorial</i>")
 slider_capacitance = ipywidgets.FloatSlider(value=1,min=0,max=3,step=0.1,description='Capacitance',readout_format='.1f',continuous_update=False)
-slider_cond_Na     = ipywidgets.FloatSlider(value=120,min=80,max=160,step=0.1,description='Sodium',readout_format='.1f',continuous_update=False)
+slider_cond_Na     = ipywidgets.FloatSlider(value=120,min=0,max=160,step=0.1,description='Sodium',readout_format='.1f',continuous_update=False)
 slider_cond_K      = ipywidgets.FloatSlider(value=36,min=0,max=80,step=0.1,description='Potassium',readout_format='.1f',continuous_update=False)
 slider_cond_L      = ipywidgets.FloatSlider(value=0.3,min=0,max=1,step=0.1,description='Leak',readout_format='.1f',continuous_update=False)
 slider_pot_Na      = ipywidgets.FloatSlider(value=50,min=-100,max=100,step=0.1,description='Sodium',readout_format='.1f',continuous_update=False)
@@ -44,6 +51,9 @@ slider_pot_L       = ipywidgets.FloatSlider(value=-54.387,min=-100,max=100,step=
 time_start         = ipywidgets.FloatText(value=0,description='Start Time',disabled=True)
 time_end           = ipywidgets.FloatText(value=450,description='Total Time',disabled=False)
 time_step          = ipywidgets.FloatText(value=0.01,description='Time Step',disabled=False)
+slider_amplitude   = ipywidgets.FloatSlider(value=50,min=-20,max=200,step=0.1,description='Amplitude',readout_format='.1f',continuous_update=False)
+slider_width       = ipywidgets.FloatSlider(value=0,min=0,max=500,step=0.1,description='Width',readout_format='.1f',continuous_update=False)
+slider_translation = ipywidgets.FloatSlider(value=50,min=0,max=250,step=0.1,description='Start at',readout_format='.1f',continuous_update=False)
 
 reset_button = ipywidgets.Button(description="Reset All")
 reset_button.on_click(resetTodefault)
@@ -56,11 +66,8 @@ h5=ipywidgets.HBox([header_potential])
 h6=ipywidgets.HBox([slider_pot_Na,slider_pot_K,slider_pot_L])
 h7=ipywidgets.HBox([header_simTime])
 h8=ipywidgets.HBox([time_start,time_end,time_step])
-h9=ipywidgets.HBox([reset_button])
-basicInputs=ipywidgets.VBox([h1,h2,h3,h4,h5,h6,h7,h8,h9])
-
-#Advanced Inputs
-cb=ipywidgets.Checkbox(value=False,description=f"<b><font color='blue'>Advanced Input - Incjection Current</b>",indent=False)
-slider_amplitude   = ipywidgets.FloatSlider(value=50,min=-100,max=100,step=1,description='Amplitude',readout_format='d',continuous_update=False)
-slider_width       = ipywidgets.FloatSlider(value=0,min=0,max=500,step=1,description='Width',readout_format='d',continuous_update=False)
-slider_translation = ipywidgets.FloatSlider(value=50,min=0,max=250,step=1,description='Start at',readout_format='d',continuous_update=False)
+h9=ipywidgets.HBox([header_injCurrent])
+h10=ipywidgets.HBox([injCurrent_note])
+h11=ipywidgets.HBox([slider_amplitude,slider_width,slider_translation])
+h12=ipywidgets.HBox([reset_button])
+modelInputs=ipywidgets.VBox([h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12])
