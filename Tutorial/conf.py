6260b610863542174dccd9157088f0f9e9e8b38c
pgleeson <p.gleeson@gmail.com>
2023-09-08 15:05:33 +0100

    Update year in footer; update GHA versions

--- Tutorial/conf.py
+++ Tutorial/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Hodgkin Huxley Tutorial'
-copyright = u'2014-2022, Joe Bowen & OpenWorm project'
+copyright = u'2014-2023, Joe Bowen & OpenWorm project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
